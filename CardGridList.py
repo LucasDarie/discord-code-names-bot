741aec4a860f6a6c35532e238be2b20a256492ef
Lucas Darie <lucas.darie@etu.umontpellier.fr>
2023-02-27 04:19:32 +0100

    create CardGridList with create, delete and get methods

---
+++ CardGridList.py
@@ -0,0 +1,26 @@
+from CardGrid import CardGrid
+from Game import Game
+from Language import Language
+from CodeGameExceptions import GameInChannelAlreadyCreated, GameNotFound
+
+class GameList(object):
+    def __init__(self) -> None:
+        self.game_list :dict[str:Game]= {}
+
+    async def create_game(self, language:Language, channel_id:str, creator_id:str):
+        if channel_id in self.game_list:
+            raise GameInChannelAlreadyCreated()
+        newGame = Game(language, creator_id, channel_id)
+        self.game_list[channel_id] = newGame
+        return newGame
+
+    async def delete_game(self, channel_id):
+        if channel_id in self.game_list:
+            raise GameNotFound()
+        self.game_list.pop(channel_id)
+    
+    def get_game(self, channel_id):
+        if channel_id not in self.game_list:
+            raise GameNotFound()
+        return self.game_list.get(channel_id)
+        
