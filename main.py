cec60d6041bd4d61e40e856d95ed57f2b97f7181
Lucas Darie <lucas.darie@etu.umontpellier.fr>
2023-03-01 05:32:08 +0100

    creating all the command and all works well :D v1.0

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import asyncio
 from Language import Language
 from ColorCard import ColorCard
 from CodeGameExceptions import *
+from Game import Game, State
 
 load_dotenv()
 
@@ -17,6 +18,196 @@ bot = interactions.Client(token=BOT_TOKEN, default_scope=GUILD_ID)
 GAME_LIST = GameList()
 
 
+@bot.user_command(name="User Command")
+async def test(ctx: interactions.CommandContext):
+    await ctx.send(f"You have applied a command onto user {ctx.target.user.username}!")
+
+
+
+@bot.command()
+async def display(ctx: interactions.CommandContext):
+    """Display the grid depending on your role"""
+    try:
+        game = await GAME_LIST.get_game(ctx.channel_id)
+        image = interactions.File(game.get_user_image_path(ctx.user))
+        await ctx.send(files=image, ephemeral=True)
+    except GameNotFound:
+        await ctx.send("No game created. Use `/create` to create a game !", ephemeral=True)
+    except GameNotStarted:
+        await ctx.send("The game did not start")
+    except NotInGame:
+        await ctx.send("You are not in the game")
+    except FileNotFoundError:
+        await ctx.send("Error : Image not found")
+
+def get_create_message(game:Game):
+    return f"({game.language.value}) Game created by <@{game.creator_id}>\
+                \n{ColorCard.BLUE.value} team : {[p.user.username for p in game.teams[ColorCard.BLUE]]}\
+                \n{ColorCard.RED.value} team : {[p.user.username for p in game.teams[ColorCard.RED]]}"
+
+def get_join_buttons() -> list[interactions.Button]:
+    return [
+        interactions.Button(
+            custom_id="blue_join_button_id",
+            style=interactions.ButtonStyle.PRIMARY,
+            label=f"{ColorCard.BLUE.value} TEAM!",
+        ),
+        interactions.Button(
+            custom_id="red_join_button_id",
+            style=interactions.ButtonStyle.DANGER,
+            label=f"{ColorCard.RED.value} TEAM!",
+        ),
+        interactions.Button(
+            custom_id="leave_button_id",
+            style=interactions.ButtonStyle.SECONDARY,
+            label=f"LEAVE GAME",
+        ),
+        ]
+
+@bot.command()
+@interactions.option(
+    description="Chose the language of the game",
+    type=interactions.OptionType.STRING,
+    name="language",
+    required=True,
+    choices=[
+        interactions.Choice(name="French", value=Language.FR.value),
+        interactions.Choice(name="English", value=Language.EN.value)
+    ] 
+)
+async def create(ctx: interactions.CommandContext, language:str):
+    """Create a game of Code Names"""
+    lang = Language.FR if language == Language.FR.value else Language.EN
+    try:
+        game = await GAME_LIST.create_game(language=lang, channel_id=ctx.channel_id, creator_id=ctx.user.id, guild_id=ctx.guild_id)
+        await ctx.send(content=get_create_message(game), components=get_join_buttons())
+    except GameInChannelAlreadyCreated:
+        await ctx.send("A game is already created in this channel", ephemeral=True)
+
+@bot.command()
+async def delete(ctx: interactions.CommandContext):
+    """Create a game of Code Names"""
+    try:
+        await GAME_LIST.delete_game(channel_id=ctx.channel_id)
+        await ctx.send(f"{ctx.user.username} deleted the game")
+    except GameNotFound:
+        await ctx.send("There is currently no game in this channel", ephemeral=True)
+
+
+async def join_team(
+        ctx: interactions.ComponentContext | interactions.CommandContext, 
+        team:ColorCard, 
+        message:interactions.Message=None):
+    color = ColorCard.BLUE if team == ColorCard.BLUE.value else ColorCard.RED
+    try:
+        game = await GAME_LIST.get_game(ctx.channel_id)
+        await game.join(ctx.user, team_color=color)
+        if(message != None):
+            await message.edit(content=get_create_message(game), components=get_join_buttons())
+            await ctx.send(f"You joined the {color.value} team !", ephemeral=True)
+        else:
+            await ctx.send(f"{ctx.user.username} join the {color.value} team !")
+    except GameNotFound:
+        await ctx.send("No game created. Use `/create` to create a game !", ephemeral=True)
+    except GameAlreadyStarted:
+        await ctx.send("The game has already started")
+
+
+
+@bot.command()
+@interactions.option(
+    description="Chose your team !",
+    type=interactions.OptionType.STRING,
+    name="team",
+    required=True,
+    choices=[
+        interactions.Choice(name="🔵 BLUE", value=ColorCard.BLUE.value),
+        interactions.Choice(name="🔴 RED", value=ColorCard.RED.value)
+    ] 
+)
+async def join(ctx: interactions.CommandContext, team:str):
+    """Join a game of Code Names"""
+    await join_team(ctx, team)
+
+@bot.component("red_join_button_id")
+async def on_login_red_click(ctx:interactions.ComponentContext):
+    await join_team(ctx, team=ColorCard.RED.value, message=ctx.message)
+
+@bot.component("blue_join_button_id")
+async def on_login_blue_click(ctx:interactions.CommandContext):
+    await join_team(ctx, team=ColorCard.BLUE.value, message=ctx.message)
+
+
+
+@bot.command()
+@bot.component("leave_button_id")
+async def leave(ctx: interactions.CommandContext):
+    """leave a game that did not start"""
+    try:
+        game = await GAME_LIST.get_game(ctx.channel_id)
+        await game.leave(ctx.user)
+        if(ctx.message != None):
+            await ctx.message.edit(content=get_create_message(game), components=get_join_buttons())
+            await ctx.send(f"You left the game", ephemeral=True)
+        else:
+            await ctx.send(f"{ctx.user.username} left the game")
+    except GameNotFound:
+        await ctx.send("No game created. Use `/create` to create a game !", ephemeral=True)
+    except NotInGame:
+        await ctx.send("You are not in the game", ephemeral=True)
+    except GameAlreadyStarted:
+        await ctx.send("The game has already started")
+
+@bot.command()
+async def start(ctx: interactions.CommandContext):
+    """Start the game of Code Names"""
+    try:
+        game = await GAME_LIST.get_game(ctx.channel_id)
+        await game.start(ctx.user.id)
+        color = game.starting_team_color
+        image = interactions.File(game.get_image_path())
+        blue_spy, red_spy = game.spies
+        await ctx.send(f"Let's begin! {color.value} TEAM STARTS!\
+                       \n🔵 {ColorCard.BLUE.value} SPY: <@{blue_spy.user.id}>\
+                       \n🔴 {ColorCard.RED.value} SPY: <@{red_spy.user.id}>", 
+                       files=image
+        )
+    except GameNotFound:
+        await ctx.send("No game created. Use `/create` to create a game !", ephemeral=True)
+    except NotGameCreator:
+        await ctx.send("You are not the game creator, you can not start the game", ephemeral=True)
+    except GameAlreadyStarted:
+        await ctx.send("The game has already started", ephemeral=True)
+    except NotEnoughPlayerInTeam:
+        await ctx.send("Both teams need to have at least 2 players each", ephemeral=True)
+
+@bot.command()
+@interactions.option(description="A hint that will help your teammates to find the words of your color. Use `/rules` for more info")
+@interactions.option(description="The number of tries your teammates will have to find the words")
+async def suggest(ctx: interactions.CommandContext, hint:str, number_of_try:int):
+    """Suggest a hint to help your team to find words. Provide also a number of try"""
+    try:
+        game = await GAME_LIST.get_game(ctx.channel_id)
+        (word, number) = await game.suggest(ctx.user, hint, number_of_try)
+
+        await ctx.send(f"{ctx.user.username} suggest :\nHint: `{word}`\nNumber of try: `{number}`")
+
+    except GameNotFound:
+        await ctx.send("No game created. Use `/create` to create a game !", ephemeral=True)
+    except GameNotStarted:
+        await ctx.send("The game did not start", ephemeral=True)
+    except NotInGame:
+        await ctx.send("You are not in the game", ephemeral=True)
+    except NotYourRole:
+        await ctx.send("You are not a spy you can not use this command !", ephemeral=True)
+    except NotYourTurn as e:
+        await ctx.send(f"{e}", ephemeral=True)
+    except WrongHintNumberGiven:
+        await ctx.send("the `number of try` given is smaller than 0, it must be `> 0`", ephemeral=True)
+    except WordInGrid:
+        await ctx.send("The `hint` provided is present in the grid. `/rules` for more info", ephemeral=True)
+
+
 
 @bot.command()
 async def find(ctx: interactions.CommandContext):
@@ -25,46 +216,68 @@ async def find(ctx: interactions.CommandContext):
 
 @find.subcommand()
 @interactions.option(description="Find a word by using the suggested hint")
-async def word(ctx: interactions.CommandContext, option: str = None):
+async def word(ctx: interactions.CommandContext, word: str):
     """Propose a word present in the grid"""
-    await ctx.send(f"You selected the word sub command and put in {option}")
+    await find_by_func(ctx=ctx, word=word)
+    
 
 @find.subcommand()
 @interactions.option(description="A descriptive description")
-async def number(ctx: interactions.CommandContext, second_option: int):
+async def card_id(ctx: interactions.CommandContext, card_id: int):
     """Propose a word card number present in the grid"""
-    await ctx.send(f"You selected the number sub command and put in {second_option}")
+    await find_by_func(ctx=ctx, card_id=card_id)
 
-@bot.user_command(name="User Command")
-async def test(ctx: interactions.CommandContext):
-    await ctx.send(f"You have applied a command onto user {ctx.target.user.username}!")
 
-@bot.command()
-async def display(ctx: interactions.CommandContext):
-    """Display the grid depending on your role"""
-    await ctx.send("Hi there!")
-
-@bot.command()
-@interactions.option(description="Chose the language of the game")
-async def create(ctx: interactions.CommandContext, language:str):
-    """Create a game of Code Names"""
-    lang = Language.FR if language == "FR" else Language.EN
-    try:
-        await GAME_LIST.create_game(language=lang, channel_id=ctx.channel_id, creator_id=ctx.user.id, guild_id=ctx.guild_id)
-        await ctx.send(f"({lang.value}) Game created by user {ctx.user.id} in channel {ctx.channel.id}")
-    except GameInChannelAlreadyCreated:
-        ctx.send("A game is already created")
-        
-@bot.command()
-@interactions.option(description="Chose your team !")
-async def join(ctx: interactions.CommandContext, team:str):
-    """Join a game of Code Names"""
-    color = ColorCard.BLUE if team == "BLUE" else ColorCard.RED
+async def find_by_func(ctx: interactions.CommandContext, word:str=None, card_id:int=None):
     try:
         game = await GAME_LIST.get_game(ctx.channel_id)
-        await game.join(ctx.user, team_color=color)
-        await ctx.send(f"You join the {color.value} team !", ephemeral=True)
+        if word == None and card_id != None:
+            (card_color, word_found) = await game.find_by_card_id(ctx.user, card_id)
+        elif word != None and card_id == None:
+            (card_color, word_found) = await game.find_by_word(ctx.user, word)
+        else:
+            # can't happen
+            await ctx.send("An error occured. Try again")
+            return
+        image = interactions.File(game.get_image_path())
+        await ctx.send(f"The word {word_found} was {card_color.value}", files=image)
+
+        if game.state in [State.BLUE_WIN, State.RED_WIN]:
+            await ctx.send(f"{game.state.color().value} TEAM WIN!")
+            await GAME_LIST.delete_game(game.channel_id)
     except GameNotFound:
         await ctx.send("No game created. Use `/create` to create a game !", ephemeral=True)
+    except GameNotStarted:
+        await ctx.send("The game did not start", ephemeral=True)
+    except NotInGame:
+        await ctx.send("You are not in the game", ephemeral=True)
+    except NotYourRole:
+         await ctx.send("You are a spy you can not use this command !", ephemeral=True)
+    except NotYourTurn as e:
+        await ctx.send(f"{e}", ephemeral=True)
+    except WordNotInGrid:
+        await ctx.send("The the provided is not in the grid", ephemeral=True)
+
+@bot.command()
+async def skip(ctx: interactions.CommandContext):
+    """End the player turn if at least one word is proposed"""
+    try:
+        game = await GAME_LIST.get_game(ctx.channel_id)
+        await game.skip(ctx.user)
+        color = game.starting_team_color
+        image = interactions.File(game.get_image_path())
+        await ctx.send(f"{ctx.user.username} have skipped their turn...")
+    except GameNotFound:
+        await ctx.send("No game created. Use `/create` to create a game !", ephemeral=True)
+    except GameNotStarted:
+        await ctx.send("The game did not start", ephemeral=True)
+    except NotInGame:
+        await ctx.send("You are not in the game", ephemeral=True)
+    except NotYourRole:
+         await ctx.send("You are a spy you can not use this command !", ephemeral=True)
+    except NotYourTurn as e:
+        await ctx.send(f"{e}", ephemeral=True)
+    except NoWordFound:
+        await ctx.send("You must find at least one word in the grid", ephemeral=True)
 
 bot.start()
