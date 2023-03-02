9e5169274fa604276ea989462b85f6a65463d23d
Lucas Darie <lucas.darie@etu.umontpellier.fr>
2023-03-02 06:07:35 +0100

    adding a display method returning a flag for discord

--- Language.py
+++ Language.py
@@ -2,3 +2,12 @@ import enum
 class Language(enum.Enum):
    FR = "fr"
    EN = "en"
+
+   def display(self) -> str:
+      match self:
+         case Language.FR:
+            return ":flag_fr:"
+         case Language.EN:
+            return ":flag_gb:"
+         case _:
+            return ""
