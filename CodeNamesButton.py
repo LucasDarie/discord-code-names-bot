fe680cfd56f308bec38035e5e5199d73a181ec7e
Lucas Darie <lucas.darie@etu.umontpellier.fr>
2023-03-21 13:31:04 +0100

    add spy and player interactions buttons

--- CodeNamesButton.py
+++ CodeNamesButton.py
@@ -15,6 +15,8 @@ class CodeNamesButton(enum.Enum):
    START_BUTTON = "start_button_id"
    SKIP_BUTTON = "skip_button_id"
    DISPLAY_GRID_BUTTON = "display_grid_button_id"
+   SPY_INTERACTION_BUTTON = "spy_interaction_button_id"
+   PLAYER_INTERACTION_BUTTON = "player_interaction_button_id"
 
    def label(self, language:Language):
       match language:
@@ -38,6 +40,10 @@ class CodeNamesButton(enum.Enum):
                   return "PASSER SON TOUR"
                case CodeNamesButton.DISPLAY_GRID_BUTTON:
                   return "AFFICHER LA GRILLE"
+               case CodeNamesButton.SPY_INTERACTION_BUTTON:
+                  return "PROPOSER UN INDICE"
+               case CodeNamesButton.PLAYER_INTERACTION_BUTTON:
+                  return "DEVINER UN MOT"
          case _:
             match self:
                case CodeNamesButton.BLUE_JOIN_BUTTON:
@@ -58,6 +64,10 @@ class CodeNamesButton(enum.Enum):
                   return "SKIP"
                case CodeNamesButton.DISPLAY_GRID_BUTTON:
                   return "DISPLAY GRID"
+               case CodeNamesButton.SPY_INTERACTION_BUTTON:
+                  return "SUGGEST AN HINT"
+               case CodeNamesButton.PLAYER_INTERACTION_BUTTON:
+                  return "GUESS A WORD"
 
    @classmethod
    def get_by_color(cls, color:ColorCard) -> CodeNamesButton:
@@ -76,63 +86,83 @@ class CodeNamesButton(enum.Enum):
          
 
 def get_display_button(language:Language) -> list[interactions.Button]:
-    return [interactions.Button(
-            custom_id=CodeNamesButton.DISPLAY_GRID_BUTTON.value,
-            style=interactions.ButtonStyle.SUCCESS,
-            label=CodeNamesButton.DISPLAY_GRID_BUTTON.label(language),
-        )]
+   return [interactions.Button(
+         custom_id=CodeNamesButton.DISPLAY_GRID_BUTTON.value,
+         style=interactions.ButtonStyle.SUCCESS,
+         label=CodeNamesButton.DISPLAY_GRID_BUTTON.label(language),
+      )]
+
+def get_spy_interaction_button(language:Language, team_color:ColorCard) -> list[interactions.Button]:
+   return [interactions.Button(
+            custom_id=CodeNamesButton.SPY_INTERACTION_BUTTON.value,
+            style=interactions.ButtonStyle.SECONDARY,
+            label=CodeNamesButton.SPY_INTERACTION_BUTTON.label(language),
+            emoji=team_color.get_emoji()
+      )]
+
+def get_player_interaction_button(language:Language, team_color:ColorCard) -> list[interactions.Button]:
+   return [interactions.Button(
+         custom_id=CodeNamesButton.PLAYER_INTERACTION_BUTTON.value,
+         style=interactions.ButtonStyle.SECONDARY,
+         label=CodeNamesButton.PLAYER_INTERACTION_BUTTON.label(language),
+         emoji=team_color.get_emoji()
+   )]
 
 def get_skip_button(language:Language) -> list[interactions.Button]:
-        return [interactions.Button(
-                custom_id=CodeNamesButton.SKIP_BUTTON.value,
-                style=interactions.ButtonStyle.SUCCESS,
-                label=CodeNamesButton.SKIP_BUTTON.label(language),
-            )]
+   return [interactions.Button(
+            custom_id=CodeNamesButton.SKIP_BUTTON.value,
+            style=interactions.ButtonStyle.SUCCESS,
+            label=CodeNamesButton.SKIP_BUTTON.label(language),
+      )]
 
 
 
 def state_component(game:Game) -> list[interactions.Button] | list[interactions.ActionRow] | None:
-    match game.state:
-        case State.WAITING:
+   match game.state:
+      case State.WAITING:
             return get_create_buttons(game)
-        case State.SPY:
-            return get_display_button(game.language)
-        case State.PLAYER:
+      case State.SPY:
+            liste = get_spy_interaction_button(game.language, game.color_state)
+            liste.extend(get_display_button(game.language))
+            return liste
+      case State.PLAYER:
+            liste = get_player_interaction_button(game.language, game.color_state)
             # don't display skip button after /suggest message
             if game.one_word_found:
-                return get_skip_button(game.language)
-        case _:
+               liste.extend(get_skip_button(game.language))
+            return liste
+      case _:
             return None
        
 
 def get_create_buttons(game:Game) -> list[interactions.ActionRow] | None:
-    join_buttons = [
-        interactions.Component(
-            custom_id=CodeNamesButton.get_by_color(team_color).value,
-            style=interactions.ButtonStyle.SECONDARY,
-            label=CodeNamesButton.get_by_color(team_color).label(game.language),
-            emoji=team_color.get_emoji()
-        )
-        for team_color in game.team_colors
-    ]
-    join_buttons.append(
-        interactions.Component(
-            custom_id=CodeNamesButton.SPY_BUTTON.value,
-            style=interactions.ButtonStyle.PRIMARY,
-            label=CodeNamesButton.SPY_BUTTON.label(game.language),
-            emoji=interactions.Emoji(name="🕵️‍♂️")
-        )
-    )
-    start_leave_buttons = [
-        interactions.Component(
-            custom_id=CodeNamesButton.LEAVE_BUTTON.value,
-            style=interactions.ButtonStyle.DANGER,
-            label=CodeNamesButton.LEAVE_BUTTON.label(game.language)
-        ),
-        interactions.Component(
-            custom_id=CodeNamesButton.START_BUTTON.value,
-            style=interactions.ButtonStyle.SUCCESS,
-            label=CodeNamesButton.START_BUTTON.label(game.language)
-        ),
-    ]
-    return [interactions.ActionRow(components=join_buttons), interactions.ActionRow(components=start_leave_buttons)]
+   join_buttons = [
+      interactions.Component(
+         custom_id=CodeNamesButton.get_by_color(team_color).value,
+         style=interactions.ButtonStyle.SECONDARY,
+         label=CodeNamesButton.get_by_color(team_color).label(game.language),
+         emoji=team_color.get_emoji()
+      )
+      for team_color in game.team_colors
+   ]
+   join_buttons.append(
+      interactions.Component(
+         custom_id=CodeNamesButton.SPY_BUTTON.value,
+         style=interactions.ButtonStyle.PRIMARY,
+         label=CodeNamesButton.SPY_BUTTON.label(game.language),
+         emoji=interactions.Emoji(name="🕵️‍♂️")
+      )
+   )
+   start_leave_buttons = [
+      interactions.Component(
+         custom_id=CodeNamesButton.LEAVE_BUTTON.value,
+         style=interactions.ButtonStyle.DANGER,
+         label=CodeNamesButton.LEAVE_BUTTON.label(game.language)
+      ),
+      interactions.Component(
+         custom_id=CodeNamesButton.START_BUTTON.value,
+         style=interactions.ButtonStyle.SUCCESS,
+         label=CodeNamesButton.START_BUTTON.label(game.language)
+      ),
+   ]
+   return [interactions.ActionRow(components=join_buttons), interactions.ActionRow(components=start_leave_buttons)]
