c8f5b67ab6adec605731aaf28ce0a9c71846a1ab
Lucas Darie <lucas.darie@etu.umontpellier.fr>
2023-03-26 03:07:20 +0200

    righting some Game class tests

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+from Game import Game
+from Language import Language
+from CardGrid import CardGrid
+from ColorCard import ColorCard
+from Creator import Creator
+import interactions as di
+import enum
+from CodeGameExceptions import *
